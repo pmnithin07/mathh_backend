327e28c43c8e4e473e1fb4035eb375ca5d585bf5
PMNITHIN5 <95852024+PMNITHIN5@users.noreply.github.com>
2023-04-13 17:38:38 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+// const req = require("express/lib/request");
+const app = express();
+const mongoose = require("mongoose");
+
+
+//database 
+const database = module.exports = async() => {
+     const connectParams = {
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+     }
+     try {
+        mongoose.connect(
+            "mongodb://0.0.0.0:27017",
+        connectParams
+        );
+      
+        console.log("Database connected successfully");
+        
+    } catch (error) {
+        console.log(error);
+        console.log("Database Not Connected Successfully");
+     }
+}
+database();
+
+
+//middleware
+
+app.use(express.urlencoded({
+    extended:true
+}));
+
+app.use(express.static("public"));
+app.set("view engine","ejs");
+app.use(express.json());
+
+// routes
+app.use(require("./routes/index"));
+app.use(require("./routes/todo"));
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000" );
+});
