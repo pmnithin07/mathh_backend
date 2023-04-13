08cb311dae59e7002361ce88b9828682a0cf4132
PMNITHIN5 <95852024+PMNITHIN5@users.noreply.github.com>
2023-04-13 17:44:50 +0530

    first commit

--- app.js
+++ app.js
@@ -40,6 +40,6 @@ app.use(express.json());
 app.use(require("./routes/index"));
 app.use(require("./routes/todo"));
 
-app.listen(3000,()=>{
+app.listen(5000,()=>{
     console.log("Server is running on port 3000" );
 });
